98023fafcda5023880e95b150526d32059656efe
P4tt1XD <patti@outlook.com.br>
2021-12-31 20:09:02 -0300

    Almost finished

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Components/Navbar.jsx';
+import {Link} from 'react-scroll';
+import './index.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+            <Navbar />
+
+            </header>
+        </div>
+    );
+}
+
+export default App;
